3e60e130edb5d5eee5bf661718feba8a89160538
Alexandre Direito <xandedsj@gmail.com>
2024-05-30 19:10:21 -0300

    implementação do switch-case

--- sistema/src/Usuario.java
+++ sistema/src/Usuario.java
@@ -1,37 +1,95 @@
+import java.io.IOException;
+import java.io.Console;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Usuario {
     public static void main(String[] args) throws Exception {
         
         SmartTv smartTv = new SmartTv();
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        // Estado inicial da tv:
-        System.out.println("Tv = " + smartTv.ligada);
-        System.out.println("Sintonizada no Canal " + smartTv.canal);
-        System.out.println("Volume " + smartTv.volume);
-        System.out.println("---------------------------------");
+        String menu = "---------------------------------\n" +
+        "TV Smart 2.5\n" + "Estado = " + smartTv.ligada + "\n" + "Sintonizada no Canal " + smartTv.canal +
+        "\n" + "Volume " + smartTv.volume + "\n" + 
+        "---------------------------------";
+
+        boolean exibirMenu = true;
+        while (exibirMenu) {
+
+            System.out.println("---------------------------------");
+            System.out.println("Digite a opção desejada:");
+            System.out.println("1 - Ligar tv");
+            System.out.println("2 - Escolher canal");
+            System.out.println("3 - Buscar canais");
+            System.out.println("4 - Aumentar volume");
+            System.out.println("5 - Diminuir volume");
+            System.out.println("0 - Desligar tv");
+            System.out.println("---------------------------------");
+
+            switch (scanner.nextInt()) {
+
+                case 1:
+                    smartTv.ligarTv();
+                    break;
+
+                case 2:
+                    System.out.println("Escolha o canal desejado:");
+                    smartTv.mudarCanal(scanner.nextInt());
+                    break;
+                
+                case 3:
+                    smartTv.buscarCanal();
+                    break;
+
+                case 4:
+                    smartTv.aumentarVolume();
+                    break;
+
+                case 5:
+                    smartTv.diminuirVolume();
+                    break;
+
+                case 0:
+                    smartTv.desligarTv();
+                    exibirMenu = false;
+                    break;
+
+                default:
+                    System.out.println("Comando inválido!");
+                    break;
+            }
+        }
+
+        // // Estado inicial da tv:
+        // System.out.println("Tv = " + smartTv.ligada);
+        // System.out.println("Sintonizada no Canal " + smartTv.canal);
+        // System.out.println("Volume " + smartTv.volume);
+        // System.out.println("---------------------------------");
 
 
-        // Chamada do usuário:
+        // // Chamada do usuário:
         
-        System.out.println("Alexa, ligar tv.");
-        smartTv.ligarTv();
+        // System.out.println("Alexa, ligar tv.");
+        // smartTv.ligarTv();
 
-        System.out.println("---------------------------------");
-        System.out.println("Alexa, aumentar volume em 1 duas vezes.");
-        smartTv.aumentarVolume();
-        smartTv.aumentarVolume();
+        // System.out.println("---------------------------------");
+        // System.out.println("Alexa, aumentar volume em 1 duas vezes.");
         // smartTv.aumentarVolume();
+        // smartTv.aumentarVolume();
+        // // smartTv.aumentarVolume();
 
-        System.out.println("---------------------------------");
-        System.out.println("Alexa, diminuir volume em 1.");
-        smartTv.diminuirVolume();
-        smartTv.diminuirVolume();
+        // System.out.println("---------------------------------");
+        // System.out.println("Alexa, diminuir volume em 1.");
+        // smartTv.diminuirVolume();
+        // smartTv.diminuirVolume();
 
-        System.out.println("---------------------------------");
-        System.out.println("Alexa, buscar canais.");
-        smartTv.buscarCanal();
+        // System.out.println("---------------------------------");
+        // System.out.println("Alexa, buscar canais.");
+        // smartTv.buscarCanal();
 
-        System.out.println("---------------------------------");
-        System.out.println("Alexa, colocar no canal 45.");
-        smartTv.mudarCanal(45);
+        // System.out.println("---------------------------------");
+        // System.out.println("Alexa, colocar no canal 45.");
+        // smartTv.mudarCanal(45);
     }
 }
